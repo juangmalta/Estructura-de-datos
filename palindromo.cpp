c39e777ed1fc83c12ccaee5288afd8eee0768dee
tehjeipi <72479272+tehjeipi@users.noreply.github.com>
2021-11-14 20:28:57 -0600

    yes

--- palindromo.cpp
+++ palindromo.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
+int main()
+{
     string texto;
     int aux = 0, igual = 0;
-    
+
     cout << "palabra : ";
     getline(cin >> ws, texto);
-    
-    for(int ind = texto.length() - 1; ind >= 0; ind--) {
-        if(texto[ind] == texto[aux]) {
+
+    for (int ind = texto.length() - 1; ind >= 0; ind--)
+    {
+        if (texto[ind] == texto[aux])
+        {
             igual++;
         }
         aux++;
     }
-    
-    if(texto.length() == igual) {
+
+    if (texto.length() == igual)
+    {
         cout << "Si es" << endl;
-    } else {
+    }
+    else
+    {
         cout << "No es" << endl;
     }
-    
+
     return 0;
 }
