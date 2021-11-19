c1cba185a8c6cf960cb2340a9752c11aa2570a66
tehjeipi <72479272+tehjeipi@users.noreply.github.com>
2021-11-18 19:13:08 -0600

    grafos

--- grafos1.cpp
+++ grafos1.cpp
@@ -1,78 +1,104 @@
-#include <iostream>
-#include <vector>
-
+// A C++ Program to check whether a graph is tree or not
+#include<iostream>
+#include <list>
+#include <limits.h>
 using namespace std;
 
-class MatrizDeAdyacencia {
-
-private:
-  vector < vector <int> > matrix;
-
+// Class for an undirected graph
+class Graph
+{
+	int V; // No. of vertices
+	list<int> *adj; // Pointer to an array for adjacency lists
+	bool isCyclicUtil(int v, bool visited[], int parent);
 public:
-
-  MatrizDeAdyacencia(){
-
-  }
-
-  void newNode(){
-    vector <int> arreglo;
-
-    arreglo.push_back(0);
-    for (int i = 0; i < matrix.size() - 1 ; i++){
-      arreglo.push_back(0);
-      this->matrix[i].push_back(0);
-
-    }
-
-    this->matrix.push_back(arreglo);
-
-  }
-
-  void addRelation(int primero, int segundo, int peso){
-
-    this->matrix[primero][segundo] = peso;
-      // Solo si es bidireccional
-      // matrix[segundo][primero] = peso;
-
-  }
-
-  void deleteRelation(int primero, int segundo){
-    if (primero >= 0 && primero < this->matrix.size() && segundo >= 0 && segundo < this->matrix.size()){
-      this->matrix[primero][segundo] = 0;
-      // Solo si es bidireccional
-      // matrix[segundo][primero] = 0;
-    }
-  }
-
-  void print(){
-    for (int i = 0; i < this->matrix.size() ; i++){
-      for (int j = 0; j < this->matrix.size() ; j++){
-        cout << this->matrix[i][j];
-      }
-      cout << endl;
-    }
-
-  }
+	Graph(int V); // Constructor
+	void addEdge(int v, int w); // to add an edge to graph
+	bool isTree(); // returns true if graph is tree
 };
 
-int main() {
-  cout << "antes";
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
+}
 
-  MatrizDeAdyacencia ejemplo;
-  ejemplo.newNode();
-  ejemplo.newNode();
-  ejemplo.newNode();
-  ejemplo.newNode();
-  ejemplo.newNode();
-  cout << "después";
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w); // Add w to v’s list.
+	adj[w].push_back(v); // Add v to w’s list.
+}
 
-  /*
-  ejemplo.addRelation(0,3,1);
-  ejemplo.addRelation(2,1,3);
-  ejemplo.addRelation(3,1,2);
-  ejemplo.addRelation(2,3,4);
+// A recursive function that uses visited[] and parent to
+// detect cycle in subgraph reachable from vertex v.
+bool Graph::isCyclicUtil(int v, bool visited[], int parent)
+{
+	// Mark the current node as visited
+	visited[v] = true;
 
-  ejemplo.print();
+	// Recur for all the vertices adjacent to this vertex
+	list<int>::iterator i;
+	for (i = adj[v].begin(); i != adj[v].end(); ++i)
+	{
+		// If an adjacent is not visited, then recur for
+		// that adjacent
+		if (!visited[*i])
+		{
+		if (isCyclicUtil(*i, visited, v))
+			return true;
+		}
 
-  */
+		// If an adjacent is visited and not parent of current
+		// vertex, then there is a cycle.
+		else if (*i != parent)
+		return true;
+	}
+	return false;
+}
+
+// Returns true if the graph is a tree, else false.
+bool Graph::isTree()
+{
+	// Mark all the vertices as not visited and not part of
+	// recursion stack
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+
+	// The call to isCyclicUtil serves multiple purposes.
+	// It returns true if graph reachable from vertex 0
+	// is cyclcic. It also marks all vertices reachable
+	// from 0.
+	if (isCyclicUtil(0, visited, -1))
+			return false;
+
+	// If we find a vertex which is not reachable from 0
+	// (not marked by isCyclicUtil(), then we return false
+	for (int u = 0; u < V; u++)
+		if (!visited[u])
+		return false;
+
+	return true;
+}
+
+// Driver program to test above functions
+int main()
+{
+	Graph g1(5);
+	g1.addEdge(1, 0);
+	g1.addEdge(0, 2);
+	g1.addEdge(0, 3);
+	g1.addEdge(3, 4);
+	g1.isTree()? cout << "Graph is Tree\n":
+				cout << "Graph is not Tree\n";
+
+	Graph g2(5);
+	g2.addEdge(1, 0);
+	g2.addEdge(0, 2);
+	g2.addEdge(2, 1);
+	g2.addEdge(0, 3);
+	g2.addEdge(3, 4);
+	g2.isTree()? cout << "Graph is Tree\n":
+				cout << "Graph is not Tree\n";
+
+	return 0;
 }
